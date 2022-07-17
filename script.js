095eda9a6f660e3256af1f2c94cf3429fbacbd0a
Piotr Rogawski <rogawski.piotr@gmail.com>
2022-07-17 20:49:38 +0200

    feat/Add the rest of flex properties

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const squareSizeUnit = document.querySelectorAll(".square-size-unit");
 const squareOrder = document.querySelectorAll(".square-order");
 const squareFlexGrow = document.querySelectorAll(".square-flex-grow");
 const squareFlexShrink = document.querySelectorAll(".square-flex-shrink");
+const squareFlexBasis = document.querySelectorAll(".square-flex-basis");
+const squareFlexBasisUnit = document.querySelectorAll(".square-flex-basis-unit");
 const squareAlignSelf = document.querySelectorAll(".square-align-self");
 
 const changeContainerSize = (e) => {
@@ -44,23 +46,34 @@ const changeAlignContent = () => {
 
 const changeSquareSize = (e) => {
     const square = document.querySelector(`#${e.target.id.slice(0, 2)}square`);
-    let unit, value;
-
+    let unit, property;
 
     if(e.target.id.includes("unit")){
         unit = e.target.value;
-        value = document.querySelector(`#${e.target.id.replace('-unit', '')}`).value;
+        property = document.querySelector(`#${e.target.id.replace('-unit', '')}`);
     } else {
         unit = document.querySelector(`#${e.target.id}-unit`).value;
-        value = e.target.value;
+        property = e.target;
     }
 
     if(e.target.id.includes("width")){
-        square.style.width = value + unit;
+        if(unit === "auto"){
+            square.style.width = unit;
+            property.disabled = true;
+        } else {
+            square.style.width = property.value + unit;
+            property.disabled = false;
+        }
     }
     
     if(e.target.id.includes("height")){
-        square.style.height = value + unit;
+        if(unit === "auto"){
+            square.style.height = unit;
+            property.disabled = true;
+        } else {
+            square.style.height = property.value + unit;
+            property.disabled = false;
+        } 
     } 
 }
 
@@ -77,6 +90,29 @@ const changeSquareFlexGrow = (e) => {
 const changeSquareFlexShrink = (e) => {
     const square = document.querySelector(`#${e.target.id.slice(0, 2)}square`);
     square.style.flexShrink = e.target.value;
+}
+
+const changeSquareFlexBasis = (e) => {
+    const square = document.querySelector(`#${e.target.id.slice(0, 2)}square`);
+    let unit, property;
+
+    console.log(e);
+
+    if(e.target.id.includes("unit")){
+        unit = e.target.value;
+        property = document.querySelector(`#${e.target.id.replace('-unit', '')}`);
+    } else {
+        unit = document.querySelector(`#${e.target.id}-unit`).value;
+        property = e.target;
+    }
+
+    if(unit === "auto"){
+        square.style.flexBasis = unit;
+        property.disabled = true;
+    } else {
+        square.style.flexBasis= property.value + unit;
+        property.disabled = false;      
+    }
 }
 
 const changeSquareAlignSelf = (e) => {
@@ -103,21 +139,32 @@ squareSizeUnit.forEach(unit => {
 
 squareOrder.forEach(order => {
     order.addEventListener("change", (e) => changeSquareOrder(e));
-})
+});
 
 squareFlexGrow.forEach(flexGrow => {
     flexGrow.addEventListener("change", (e) => changeSquareFlexGrow(e));
-})
+});
 
 squareFlexShrink.forEach(flexShrink => {
     flexShrink.addEventListener("change", (e) => changeSquareFlexShrink(e));
-})
+});
+
+squareFlexBasis.forEach(flexBasis => {
+    flexBasis.addEventListener("change", (e) => changeSquareFlexBasis(e));
+});
+
+squareFlexBasisUnit.forEach(flexBasisUnit => {
+    flexBasisUnit.addEventListener("change", (e) => changeSquareFlexBasis(e));
+});
 
 squareAlignSelf.forEach(alignSelf => {
     alignSelf.addEventListener("change", (e) => changeSquareAlignSelf(e));
-})
+});
+
 
 for(let i = 0; i < markers.length; i++){
     markers[i].textContent = i + 1;
 }
+
+
 
